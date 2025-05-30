2be44e62d73544f7af46b653d50a38050ff7d979
eafonseala <esteban.fonsecalatorre@rockwellautomation.com>
2025-05-30 16:20:37 -0500

    se uso un ternary para manejar la condicion de recursividad en undefined

--- Hw04/Hw04_b.ts
+++ Hw04/Hw04_b.ts
@@ -11,7 +11,7 @@ function fibonacci(n: number): number | undefined { // Se utilizó "undefined" e
     if (n <= 1) {
         return n;
     }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    return (fibonacci(n - 1) || 0) + (fibonacci(n - 2) || 0); // se uso ternary compacto
 }
 
 const result = fibonacci(n);
